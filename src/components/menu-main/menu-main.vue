b85de509fa6a53a5bc70ba5c7cea66ec581dde7d
Evgeniy Saschenko <evgeniy.saschenko@gmail.com>
2021-11-13 22:30:20 +0200

    nav

--- src/components/menu-main/menu-main.vue
+++ src/components/menu-main/menu-main.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 include /src/assets/pug/index.pug
-el-menu(:default-active="$route.path", mode="horizontal")
-  el-menu-item(:index="item.url", v-for="item in items")
-    router-link.nav-link(:to="item.url") {{ item.name }}
++b.EL-MENU.menu-main(:default-active="$route.path", mode="horizontal")
+  +e.EL-MENU-ITEM.item(:index="item.url", v-for="item in items")
+    +e.ROUTER-LINK.link(:to="item.url") {{ item.name }}
 </template>
 
 <script lang="ts">
@@ -20,5 +20,11 @@ export default {
 </script>
 
 <style lang="sass">
-@import "@/assets/style/style.sass"
+.menu-main
+  &__item
+    padding: 0 !important
+  &__link
+    height: 100%
+    display: block
+    padding: 0 20px
 </style>
